d70c5679ca1917a3ee10ec49f68985146bda4595
Vincent <agpheaddikilla@yahoo.de>
2017-08-25 21:01:31 +0200

    fixed bank pre init loading

--- @LiveInLifeClient/addons/bank/config.cpp
+++ @LiveInLifeClient/addons/bank/config.cpp
@@ -6,3 +6,11 @@ class CfgPatches {
 class CfgFunctions {
     #include "CfgFunctions.cpp"
 };
+
+class Extended_PreInit_EventHandlers
+{
+    class lilc_bank_preInit
+    {
+        clientInit = "call compile preProcessFileLineNumbers '\x\lilc\addons\bank\XEH_clientPreInit.sqf';";
+    };
+};
