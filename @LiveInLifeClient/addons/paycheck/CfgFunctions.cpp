5803bf4dc74b520dcd772fa39b2b55504ec45e6d
Vincent <agpheaddikilla@yahoo.de>
2017-08-25 23:36:17 +0200

    fixed paycheck startup error message

--- @LiveInLifeClient/addons/paycheck/CfgFunctions.cpp
+++ @LiveInLifeClient/addons/paycheck/CfgFunctions.cpp
@@ -2,8 +2,6 @@
 class lilc_paycheck {
     class functions {
         file = "\x\lilc\addons\paycheck\functions";
-        class addPaycheck {};
         class init {};
-        class removePaycheck {};
     };
 };
