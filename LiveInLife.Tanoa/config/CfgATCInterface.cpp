6809a88960082e73cc6d1d115fa4d4db0f516f71
XeonDiaboli <31048254+XeonDiaboli@users.noreply.github.com>
2017-08-25 18:36:07 +0200

    Update CfgATCInterface.cpp

--- LiveInLife.Tanoa/config/CfgATCInterface.cpp
+++ LiveInLife.Tanoa/config/CfgATCInterface.cpp
@@ -1,6 +1,12 @@
 CfgATCInterface {
-  classnames[] = {
-                  {"sab_ultralight", 130, 0},
-                  {"MH-900", 80, 1}
+  classnames[] = {    
+                  
+                  {"sab_l19_BASE", 40, 1},
+                  {"sab_an2_BASE",40, 1},
+                  {"Plane_Fighter_01_Base_F",20, 1},
+                  {"Plane_Fighter_02_Base_F",20, 1},
+                  {"sab_C130_J_Base",80, 1},
+                  {"sab_ultralight_BASE", 25, 0},
+                  {"Heli_Light_01_base_F", 35, 1}
                };
 };
