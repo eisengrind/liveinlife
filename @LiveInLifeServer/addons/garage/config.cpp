77536b04a02e1910d6c74d62e4a229d20441af17
Vincent <agpheaddikilla@yahoo.de>
2017-11-10 14:12:34 +0100

    fixed lilc_garage

--- @LiveInLifeServer/addons/garage/config.cpp
+++ @LiveInLifeServer/addons/garage/config.cpp
@@ -6,9 +6,3 @@ class CfgPatches {
 class CfgFunctions {
     #include "CfgFunctions.cpp"
 };
-
-class Extended_PreInit_EventHandlers {
-    class lils_garage_preInit {
-        serverInit = "call compile preProcessFileLineNumbers '\x\lils\addons\garage\XEH_preInit.sqf';";
-    };
-};
