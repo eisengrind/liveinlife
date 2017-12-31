a9c6fac6c65395d15deb3aad52bbad8c166dac13
Vincent <agpheaddikilla@yahoo.de>
2017-12-31 22:35:35 +0100

    fixed ace release action

--- @LiveInLifeClient/addons/prison/CfgVehicles.cpp
+++ @LiveInLifeClient/addons/prison/CfgVehicles.cpp
@@ -9,15 +9,15 @@ class CAManBase : Man
             class lilc_prison_addPunishment
             {
                 displayName = "$STR_lilc_prison_ActionTitle_addPunishment";
-                condition = "[_target] call lilc_prison_fnc_canAddPunishment";
+                condition = "([_target] call lilc_prison_fnc_canAddPunishment)";
                 statement = "[_target] call lilc_prison_fnc_doAddPunishment;";
             };
 
             class lilc_prison_endPunishment
             {
                 displayName = "$STR_lilc_prison_ActionTitle_endPunishment";
-                condition = "[_target] call lilc_prison_fnc_canEndPunishment";
-                statement = "[_target] call lilc_prison_fnc_doEndPunishment;";
+                condition = "([_target] call lilc_prison_fnc_canRelease)";
+                statement = "[_target] call lilc_prison_fnc_doRelease;";
             };
         };
     };
