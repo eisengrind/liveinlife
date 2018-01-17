6a4cb6037b014742410e7205d6b32954ce16b50e
Vincent <agpheaddikilla@yahoo.de>
2018-01-17 21:23:54 +0100

    changed some settings

--- LiveInLife.Tanoa/config/CfgVirtualItems.cpp
+++ LiveInLife.Tanoa/config/CfgVirtualItems.cpp
@@ -11,4 +11,76 @@ class CfgVirtualItems {
 		modifyDescription = 1;
 		vehicleClassname = "";
 	};
+
+    class lilcvi_hairCollected {
+        displayName = "Haar";
+        descriptionShort = "Haar";
+        description = "Ist ein Haar.";
+        weight = 0;
+        defaultData[] = {}; //or something else - value will be evaluated
+        modifyDisplayName = 0;
+        modifyDescriptionShort = 0;
+        modifyDescription = 0;
+        vehicleClassname = "";
+    };
+
+    class lilcvi_fingerprintCollected {
+        displayName = "Fingerabdruck";
+        descriptionShort = "Fingerabdruck";
+        description = "Ist ein Fingerabdruck.";
+        weight = 0;
+        defaultData[] = {}; //or something else - value will be evaluated
+        modifyDisplayName = 0;
+        modifyDescriptionShort = 0;
+        modifyDescription = 0;
+        vehicleClassname = "";
+    };
+
+    class lilcvi_bloodCollected {
+        displayName = "Blut";
+        descriptionShort = "Blut";
+        description = "Ist ein Blut.";
+        weight = 0;
+        defaultData[] = {}; //or something else - value will be evaluated
+        modifyDisplayName = 0;
+        modifyDescriptionShort = 0;
+        modifyDescription = 0;
+        vehicleClassname = "";
+    };
+
+    class lilcvi_flakeCollected {
+        displayName = "Schuppen";
+        descriptionShort = "Schuppen";
+        description = "Ist ein Schuppen.";
+        weight = 0;
+        defaultData[] = {}; //or something else - value will be evaluated
+        modifyDisplayName = 0;
+        modifyDescriptionShort = 0;
+        modifyDescription = 0;
+        vehicleClassname = "";
+    };
+
+    class lilcvi_urineCollected {
+        displayName = "Urin";
+        descriptionShort = "Urin";
+        description = "Ist ein Urin.";
+        weight = 0;
+        defaultData[] = {}; //or something else - value will be evaluated
+        modifyDisplayName = 0;
+        modifyDescriptionShort = 0;
+        modifyDescription = 0;
+        vehicleClassname = "";
+    };
+
+    class lilcvi_spitCollected {
+        displayName = "Spucke";
+        descriptionShort = "Spucke";
+        description = "Ist ein Spucke.";
+        weight = 0;
+        defaultData[] = {}; //or something else - value will be evaluated
+        modifyDisplayName = 0;
+        modifyDescriptionShort = 0;
+        modifyDescription = 0;
+        vehicleClassname = "";
+    };
 };
--- tools/compile_lils.bat
+++ tools/compile_lils.bat
@@ -1,11 +1,11 @@
 
-python "D:\Users\agphe\Documents\GitHub\public\LiveInLife\tools\compile.py"^
-	"-L" "D:\Users\agphe\Documents\GitHub\public\LiveInLife\tools\lils.tmp.log"^
+python "D:\Users\agphe\Documents\git\LiveInLife\tools\compile.py"^
+	"-L" "D:\Users\agphe\Documents\git\LiveInLife\tools\lils.tmp.log"^
 	"-N" "lils_{name}"^
 	"-k" "D:\Program Files\Steam\steamapps\common\Arma 3 Tools\DSSignFile\lil0.8.7.biprivatekey"^
 	"-b"^
-	"-T" "D:\Users\agphe\Documents\GitHub\public\LiveInLife\.builds\@LiveInLifeServer\addons"^
-	"-F" "D:\Users\agphe\Documents\GitHub\public\LiveInLife\@LiveInLifeServer\addons\*"^
+	"-T" "D:\Users\agphe\Documents\git\LiveInLife\.builds\@LiveInLifeServer\addons"^
+	"-F" "D:\Users\agphe\Documents\git\LiveInLife\@LiveInLifeServer\addons\*"^
 	"-I" "*.p3d;*.pac;*.paa;*.rtm;*.sqf;*.sqs;*.bikb;*.fsm;*.wss;*.ogg;*.wav;*.fxy;*.csv;*.html;*.lip;*.txt;*.wrp;*.bisurf;*.xml;*.rvmat;*.hpp;*.jpg;*.h"^
 	"-A" "D:\Program Files\Steam\steamapps\common\Arma 3 Tools\AddonBuilder\AddonBuilder.exe"^
 	"-p" "x\lils\addons\{folder}"
