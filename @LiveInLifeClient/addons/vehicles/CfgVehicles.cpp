9c41458f90d39798529b1909e7953bbb8cc2bc36
Vincent <agpheaddikilla@yahoo.de>
2018-02-08 22:24:14 +0100

    added vehicle information to aircrafts and ships

--- @LiveInLifeClient/addons/vehicles/CfgVehicles.cpp
+++ @LiveInLifeClient/addons/vehicles/CfgVehicles.cpp
@@ -5,7 +5,7 @@ class Car: LandVehicle {
         class ACE_MainActions {
             class lilc_vehicles_getInformation {
                 displayName = "Fahrzeuginformationen";
-                condition = "[_target] call lilc_vehicles_fnc_canShowVehicleInformation;";
+                condition = "[_target] call lilc_vehicles_fnc_canShowVehicleInformation";
                 statement = "[_target] call lilc_vehicles_fnc_doShowVehicleInformation;";
             };
         };
@@ -15,10 +15,27 @@ class Car: LandVehicle {
 
 class Air;
 class Plane: Air {
+    class ACE_Actions {
+        class ACE_MainActions {
+            class lilc_vehicles_getInformation {
+                displayName = "Fahrzeuginformationen";
+                condition = "[_target] call lilc_vehicles_fnc_canShowVehicleInformation";
+                statement = "[_target] call lilc_vehicles_fnc_doShowVehicleInformation;";
+            };
+        };
+    };
 };
 
 
 class Ship;
 class Ship_F: Ship {
-    class ACE_Actions {};
+    class ACE_Actions {
+        class ACE_MainActions {
+            class lilc_vehicles_getInformation {
+                displayName = "Fahrzeuginformationen";
+                condition = "[_target] call lilc_vehicles_fnc_canShowVehicleInformation";
+                statement = "[_target] call lilc_vehicles_fnc_doShowVehicleInformation;";
+            };
+        };
+    };
 };
