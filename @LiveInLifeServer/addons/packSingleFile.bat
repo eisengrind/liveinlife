bd1febc09f8ad9de59051dd6f27dc77c747a1073
Vincent <agpheaddikilla@yahoo.de>
2018-02-09 18:35:23 +0100

    updated packSingleFile.bat

--- @LiveInLifeServer/addons/packSingleFile.bat
+++ @LiveInLifeServer/addons/packSingleFile.bat
@@ -2,7 +2,7 @@
 "D:\Users\agphe\Documents\git\LiveInLife\tools\compile.py"^
 	"--logfile" "D:\Users\agphe\Documents\git\LiveInLife\tools\lils.tmp.log"^
 	"--name" "lils_{folder}"^
-	"--key" "D:\Program Files\Steam\steamapps\common\Arma 3 Tools\DSSignFile\lil0.8.7.biprivatekey"^
+	"--key" "D:\Users\agphe\Documents\git\LiveInLife\.builds\keys\lil0.10-g8885192.biprivatekey"^
 	"--binarize"^
 	"--pathTo" "D:\Users\agphe\Documents\git\LiveInLife\.builds\@LiveInLifeServer\addons"^
 	"--pathFrom" %1^
