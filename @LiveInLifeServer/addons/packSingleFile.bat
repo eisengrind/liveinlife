bdef5385b9a0ff63b9b39575b5fe4e17c642be25
Vincent <agpheaddikilla@yahoo.de>
2018-03-10 13:34:58 +0100

    changed private key

--- @LiveInLifeServer/addons/packSingleFile.bat
+++ @LiveInLifeServer/addons/packSingleFile.bat
@@ -2,7 +2,7 @@
 "D:\Users\agphe\Documents\git\LiveInLife\tools\compile.py"^
 	"--logfile" "D:\Users\agphe\Documents\git\LiveInLife\tools\lils.tmp.log"^
 	"--name" "lils_{folder}"^
-	"--key" "D:\Users\agphe\Documents\git\LiveInLife\.builds\keys\lil0.10-g8885192.biprivatekey"^
+	"--key" "D:\Users\agphe\Documents\git\LiveInLife\.builds\keys\lil0.10-g9e2c71e.biprivatekey"^
 	"--binarize"^
 	"--pathTo" "D:\Users\agphe\Documents\git\LiveInLife\.builds\@LiveInLifeServer\addons"^
 	"--pathFrom" %1^
