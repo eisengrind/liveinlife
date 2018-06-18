60b3de2b3cf0b1090256d621ebd5c3440d90f343
Vincent <agpheaddikilla@yahoo.de>
2018-06-18 23:45:01 +0200

    update drug config

--- @LiveInLifeClient/addons/drugs/config.cpp
+++ @LiveInLifeClient/addons/drugs/config.cpp
@@ -10,3 +10,15 @@ class CfgFunctions {
 class CfgWeapons {
     #include "CfgWeapons.cpp"
 };
+
+class Extended_PreInit_EventHandlers {
+    class lilc_drugs {
+        clientInit = "call preprocessFileLineNumbers '\x\lilc\addons\drugs\XEH_clientPreInit.sqf';";
+    };
+};
+
+class Extended_PostInit_EventHandlers {
+    class lilc_drugs {
+        clientInit = "call preprocessFileLineNumbers '\x\lilc\addons\drugs\XEH_clientPostInit.sqf';";
+    };
+};
